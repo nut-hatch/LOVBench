de68a58fa551bc9591595ce03247eaf6c00f5236
nut-hatch <niklas.kolbe@googlemail.com>
2020-04-13 13:43:35 +0200

    Moved configuration

---
+++ lovbench-dataset/src/main/java/experiment/configuration/IncorrectConfigurationException.java
@@ -0,0 +1,8 @@
+package experiment.configuration;
+
+public class IncorrectConfigurationException extends Exception {
+
+    public IncorrectConfigurationException(String message) {
+        super(message);
+    }
+}
